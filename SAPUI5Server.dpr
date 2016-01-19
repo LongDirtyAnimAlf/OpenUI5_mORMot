0a16baa22a88fe9b54b04861eb92c59f6bd974a3
LongDirtyAnimAlf <LongDirtyAnimAlf@gmail.com>
2016-01-19 07:44:42 +0100

    Small changes in Delphi dpr

--- SAPUI5Server.dpr
+++ SAPUI5Server.dpr
@@ -1,16 +1,15 @@
 program SAPUI5Server;
 
-{$I Synopse.inc}
-
 uses
   Forms,
-  Main,
-  SampleData;
+  Main in 'Main.pas' {Form1},
+  SampleData in 'SampleData.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
+  Application.MainFormOnTaskbar := True;
   Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
